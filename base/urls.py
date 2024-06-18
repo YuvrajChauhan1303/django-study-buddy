ecf5a28a5733492b70c3a95ba9c8178aa25c8e57
YuvrajChauhan1303 <email.yuvraj.13@gmail.com>
2024-06-18 19:13:03 +0530

    added CRUD operations

--- base/urls.py
+++ base/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 from django.http import HttpResponse
-
 from . import views
 
 urlpatterns = [ 
     path('', views.Home, name="home"),
     path('room/<str:pk>/', views.Rooms, name="room"),
+
+    path('create-room/', views.RoomCreate, name="create-room"),
+    path('update-room/<str:pk>', views.RoomUpdate, name="update-room"),
+    path('delete-room/<str:pk>', views.RoomDelete, name="delete-room"),
 ]
